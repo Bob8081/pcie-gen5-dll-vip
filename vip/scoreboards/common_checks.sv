e369afd91548e10073a9c0969c8ecc8026098419
shady abbas <144989308+shadyabbass@users.noreply.github.com>
2026-05-25 22:43:22 +0300

    Add files via upload
    adding checks "need to be cleaner" but already worked

--- vip/scoreboards/common_checks.sv
+++ vip/scoreboards/common_checks.sv
@@ -1,20 +1,243 @@
-class pcie_dll_common_checks extends uvm_subscriber #(pcie_dll_base_seq_item);
+class pcie_dll_common_checks extends uvm_object;
+
+  // TODO: remove redundant variables
   
   // registeration 
   `uvm_object_utils(pcie_dll_common_checks)
 
+  // Configurations
+  pcie_dll_partner_cfg  partner_cfg;
+  pcie_dll_my_cfg       my_cfg;
+
+
+  // all signals related checks "partner side"
+    pcie_dll_dllp_seq_item rx_dllp_item;
+    pcie_dll_tlp_seq_item  rx_tlp_item;
+
+    pcie_dlcmsm_state_e rx_prev_state;
+    pcie_dlcmsm_state_e rx_curr_state;
+    pcie_dllp_type_e    rx_prev_dllp_type;
+    pcie_dllp_type_e    rx_curr_dllp_type;
+
+  // all signals related checks "Tx side"
+    pcie_dll_dllp_seq_item tx_dllp_item;
+    pcie_dll_tlp_seq_item  tx_tlp_item;
+    
+    pcie_dlcmsm_state_e tx_prev_state;
+    pcie_dlcmsm_state_e tx_curr_state;
+    pcie_dllp_type_e    tx_prev_dllp_type;
+    pcie_dllp_type_e    tx_curr_dllp_type;
+
+    int unsigned counter_fc1;
+    int unsigned counter_fc2;
+    int unsigned prev_st_count1;
+    int unsigned prev_st_count2;
+
+
+  pcie_fc_type_e         credit_type;
+  bit                    tx_updated;
+  bit                    rx_updated;
+
+
+  function void calling_common_checks();
+    if (tx_updated && rx_updated) begin
+
+      // -------- scoreboard reporting -----------
+      `uvm_info("SCOREBOARD", "Both Tx and Rx items have been updated. Calling common checks...", UVM_LOW)
+      `uvm_info("SCOREBOARD", $sformatf("Current Tx state: %s, Previous Tx state: %s, Current Rx state: %s, Previous Rx state: %s", tx_curr_state.name(), tx_prev_state.name(), rx_curr_state.name(), rx_prev_state.name()), UVM_LOW)
+      `uvm_info("SCOREBOARD", $sformatf("Current Tx DLLP: %s, Previous Tx DLLP: %s, Current Rx DLLP: %s, Previous Rx DLLP: %s", tx_curr_dllp_type.name(), tx_prev_dllp_type.name(), rx_curr_dllp_type.name(), rx_prev_dllp_type.name()), UVM_LOW)
+      `uvm_info("SCOREBOARD", $sformatf("current Tx DLLP type: %s, current Rx DLLP type: %s", tx_curr_dllp_type.name(), rx_curr_dllp_type.name()), UVM_LOW)
+      `uvm_info("SCOREBOARD", $sformatf("counter_fc1: %0d, counter_fc2: %0d", counter_fc1, counter_fc2), UVM_LOW)
+
+      traffic_isolation_check();
+      check_symmetric_active();
+      Credit_Capture();
+      drop_packets();
+
+      tx_updated = 0;
+      rx_updated = 0;
+
+    end
+
+  endfunction
 
   
-  // ---- Main Body Task ----
-  virtual task body();
-    pcie_dll_dllp_seq_item init1_transaction;
+    // ------ checks for DLLP items - to be called from write function in scoreboard ------
 
-    `uvm_info("SEQ", "Starting InitFC1 Phased Traffic Generation...", UVM_LOW)
+    // traffic_isolation check implementation...
+
+    // note this function works for transmitted DLLP items
+    function void traffic_isolation_check ();
+
+      pcie_dll_dllp_seq_item dllp_item = tx_dllp_item;  
+
+        // Only proper DLLPs are transmitted during states
+        case (dllp_item.current_state)
+            DL_FEATURE_EXCH: begin
+                if (dllp_item.dllp_type != DLLP_FEATURE_REQ) begin
+                    `uvm_error("SB: TRAFFIC_ISOLATION", "Violation: Only FEATURE_EXCH DLLPs allowed in FEATURE_EXCH state!")
+                end
+                else begin
+                    `uvm_info("SB: TRAFFIC_ISOLATION", "Valid: FEATURE_EXCH DLLP detected in FEATURE_EXCH state.", UVM_LOW)
+                end
+            end
+
+            DL_INIT_FC1, DL_INIT_FC2: begin // note: we use ack to separate betweeninvalid dllp and invalid VC 
+                if (!(dllp_item.dllp_type[7:3] inside {DLLP_INITFC1_P_VC, DLLP_INITFC1_NP_VC, DLLP_INITFC1_CPL_VC, DLLP_INITFC2_P_VC, DLLP_INITFC2_NP_VC, DLLP_INITFC2_CPL_VC})) begin
+                    `uvm_error("SB: TRAFFIC_ISOLATION", "Violation: Only InitFC DLLPs allowed in INIT_FC states!")
+                end
+                else begin
+                    `uvm_info("SB: TRAFFIC_ISOLATION", "Valid: InitFC DLLP detected in INIT_FC state.", UVM_LOW)
+                end
+            end
+        endcase
+
+
+        // All InitFC DLLPs are strictly addressed to Virtual Channel 0 (VCO).
+        if (dllp_item.dllp_type[7:3] inside {DLLP_INITFC1_P_VC, DLLP_INITFC1_NP_VC, DLLP_INITFC1_CPL_VC, DLLP_INITFC2_P_VC, DLLP_INITFC2_NP_VC, DLLP_INITFC2_CPL_VC}) begin
+            if (dllp_item.dllp_type[2:0] != 3'b000) begin // note: make sure this bits hit VC ID in DLLP header
+                `uvm_error("SB: TRAFFIC_ISOLATION", "Violation: Only credit advertisement DLLPs allowed for Virtual Channel 0 (VCO) during InitFC states!")
+            end
+            else begin
+                `uvm_info("SB: TRAFFIC_ISOLATION", "Valid: credit advertisement DLLPs is for Virtual Channel 0 (VCO) during InitFC states!", UVM_LOW)
+            end
+        end
+
+    endfunction
+
+ 
+
+
+
+// ------------------- DATA INTEGREITY CHECKS IMPLEMENTATIONS -------------------
+
+    // to make sure that the state manager drops packets with ubnormal behavior
+    // note: need to handle that its the recieved DLLP
+
+    function void drop_packets ();
+
+        
+        bit is_valid = 0; // A single flag to evaluate the entire logic
+        int unsigned        current_st_count;
+        int unsigned        prev_st_count;
+        pcie_dlcmsm_state_e current_state     = rx_curr_state;
+        pcie_dllp_type_e    current_dllp_type = rx_curr_dllp_type;
+        pcie_dllp_type_e    prev_dllp_type    = rx_prev_dllp_type; 
+
+
+        if (current_state inside {DL_INIT_FC1, DL_INIT_FC2}) begin
+
+            if (current_state == DL_INIT_FC1) begin
+              current_st_count = counter_fc1;
+              prev_st_count    = prev_st_count1;
+            end
+            else begin
+              current_st_count = counter_fc2;
+              prev_st_count    = prev_st_count2;
+            end
+          
+          
+            // Error packet --> keep counter the same
+            if (pcie_dll_pkg::error_expector::determine_error_status(rx_dllp_item) != ERROR_FREE) begin 
+                        is_valid = (prev_st_count == current_st_count);
+            end
+            // repeated INITFC packets --> reset if 5 reptations
+            else if (current_dllp_type == prev_dllp_type) begin // repeated initfc
+                        is_valid = (prev_st_count == current_st_count);
+            end
+            // disorder INITFC packets --> zero
+            else if (   (current_dllp_type inside {DLLP_INITFC1_CPL } && prev_dllp_type inside {DLLP_INITFC1_P   })
+                      ||(current_dllp_type inside {DLLP_INITFC1_P   } && prev_dllp_type inside {DLLP_INITFC1_NP  })
+                      ||(current_dllp_type inside {DLLP_INITFC1_NP  } && prev_dllp_type inside {DLLP_INITFC1_CPL })
+                      ||(current_dllp_type inside {DLLP_INITFC2_CPL } && prev_dllp_type inside {DLLP_INITFC2_P   })
+                      ||(current_dllp_type inside {DLLP_INITFC2_P   } && prev_dllp_type inside {DLLP_INITFC2_NP  })
+                      ||(current_dllp_type inside {DLLP_INITFC2_NP  } && prev_dllp_type inside {DLLP_INITFC2_CPL }) ) begin
+                        is_valid = (current_st_count == 0);
+            end 
+
+            // INITFC1 within INITFC2 --> reset
+            else if (   (current_dllp_type inside {DLLP_INITFC1_P, DLLP_INITFC1_NP, DLLP_INITFC1_CPL} && prev_dllp_type inside {DLLP_INITFC2_P, DLLP_INITFC2_NP, DLLP_INITFC2_CPL}) ) begin
+                        is_valid = (current_st_count == prev_st_count);
+            end
+
+            else begin // normal behavior
+                    is_valid = (current_st_count == prev_st_count+1);
+                end 
+        end
+        
+        else begin // If the state is not INIT_FC no need to check state manager counter behavior
+            is_valid = 1; 
+        end
+
+
+        if (is_valid) begin
+            `uvm_info("PKT_DROP", "Valid: Correct drop/increment behavior", UVM_LOW)
+        end 
+        else begin
+            `uvm_error("PKT_DROP", "Violation: abnormal behavior in state manager packet drop/increment logic!")
+        end
+
+    endfunction : drop_packets
+
+
+    // to check that both RC and EP reach active state symmetrically
+    // TODO: need to make sure about its behavior
+    function void check_symmetric_active ();
+
+    if (tx_curr_state == DL_ACTIVE && rx_curr_state == DL_ACTIVE) begin
+        `uvm_info("symmetric_active", "Valid: both RC and EP reached active state symmetrically!", UVM_LOW)
+    end
+    else if (tx_curr_state == DL_INACTIVE && tx_prev_state == DL_ACTIVE && rx_curr_state != DL_ACTIVE) begin
+        `uvm_fatal("symmetric_active", "Violation: both RC and EP don't reach active state symmetrically!")
+    end
+    else if (rx_curr_state == DL_INACTIVE && rx_prev_state == DL_ACTIVE && tx_curr_state != DL_ACTIVE) begin
+        `uvm_fatal("symmetric_active", "Violation: both RC and EP don't reach active state symmetrically!")
+    end
+
+endfunction : check_symmetric_active
+
+
+// note: it's this function works for the received DLLP
+function void Credit_Capture ();
+
+pcie_fc_type_e           credit_type;
+bit                      no_capture_credits = 0; 
+
+
+    // Determine the credit type based on the DLLP type
+    case (rx_dllp_item.dllp_type)
+        DLLP_INITFC1_P   , DLLP_INITFC2_P   : credit_type = FC_P;
+        DLLP_INITFC1_NP  , DLLP_INITFC2_NP  : credit_type = FC_NP;
+        DLLP_INITFC1_CPL , DLLP_INITFC2_CPL : credit_type = FC_CPL;
+        default: begin
+             no_capture_credits = 1'b1; // flag to indicate that we shouldn't capture credits from this DLLP
+        end
+    endcase
+
+    // compare the actual received credits with the stored credits from peer
+    if (!no_capture_credits) begin
+        if (   (rx_dllp_item.hdr_FC     != partner_cfg.partner_credits[credit_type].hdr_limit) 
+             ||(rx_dllp_item.data_FC    != partner_cfg.partner_credits[credit_type].data_limit) 
+             ||(rx_dllp_item.hdr_scale  != partner_cfg.partner_credits[credit_type].hdr_scale) 
+             ||(rx_dllp_item.data_scale != partner_cfg.partner_credits[credit_type].data_scale) ) begin
+
+            `uvm_error("CRD_MISMATCH",$sformatf("Captured credits do not match expected values for type %s!", credit_type.name()))
+        end 
+        else begin
+            `uvm_info("CRD_MATCH", $sformatf("Captured credits match expected values for type %s.", credit_type.name()), UVM_LOW)
+        end
+    end
+
+    else begin 
+        `uvm_info("CRD_ERR", $sformatf("Received non-InitFC DLLP type %s. Cannot capture credits.", rx_dllp_item.dllp_type.name()), UVM_LOW)
+    end
+
+endfunction : Credit_Capture
 
 
   // constructor
-  function new(string name = "pcie_dll_common_checks", uvm_component parent = null);
-    super.new(name,parent);
+  function new(string name = "pcie_dll_common_checks");
+    super.new(name);
   endfunction
 
 
--- vip/scoreboards/pcie_dll_scoreboard.sv
+++ vip/scoreboards/pcie_dll_scoreboard.sv
@@ -1,16 +1,23 @@
 // Declare a specific imp suffix for the state port
 `uvm_analysis_imp_decl(_state)
 
-class pcie_dll_scoreboard extends yvm_subscriber #(pcie_dll_base_seq_item);
+`uvm_analysis_imp_decl(_tx)
+`uvm_analysis_imp_decl(_rx)
+
+class pcie_dll_scoreboard extends uvm_scoreboard;
   
   pcie_dll_role_e role;
 
-  // Track states
-  pcie_dlcmsm_state_e prev_state;
-  pcie_dlcmsm_state_e curr_state;
+   // Configurations
+  pcie_dll_partner_cfg  partner_cfg;
+  pcie_dll_my_cfg       my_cfg;
+
 
   // Analysis implementation for state transitions
-  uvm_analysis_imp_state #(pcie_dlcmsm_state_e, pcie_dll_scoreboard) state_export;
+  //uvm_analysis_imp_state #(pcie_dlcmsm_state_e, pcie_dll_scoreboard) state_export;
+  uvm_analysis_imp_tx    #(pcie_dll_base_seq_item, pcie_dll_scoreboard) tx_export; // my item
+  uvm_analysis_imp_rx    #(pcie_dll_base_seq_item, pcie_dll_scoreboard) rx_export; // partner item
+
 
   // Handle to common checks
   pcie_dll_common_checks checks;
@@ -19,26 +26,115 @@ class pcie_dll_scoreboard extends yvm_subscriber #(pcie_dll_base_seq_item);
 
   function new(string name = "pcie_dll_scoreboard", uvm_component parent = null);
     super.new(name, parent);
-    curr_state = DL_INACTIVE;
-    prev_state = DL_INACTIVE;
-    state_export = new("state_export", this);
+
+    //state_export = new("state_export", this);
+    tx_export = new("tx_export", this);
+    rx_export = new("rx_export", this);
   endfunction
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+
+    // Get config from uvm_config_db 
+    if (!uvm_config_db#(pcie_dll_partner_cfg)::get(this, "", "dyn_cfg", partner_cfg)) begin
+      `uvm_fatal("CONFIG_ERR", "Could not get pcie_dll_partner_cfg from config_db")
+    end
+
+    if (!uvm_config_db#(pcie_dll_my_cfg)::get(this, "", "my_cfg", my_cfg)) begin
+      `uvm_fatal("CONFIG_ERR", "Could not get pcie_dll_my_cfg from config_db")
+    end
+
     checks = pcie_dll_common_checks::type_id::create("checks");
+    checks.partner_cfg = partner_cfg;
+    checks.my_cfg = my_cfg;
   endfunction
   
   // This function is called automatically when the state_mgr writes to the port
-  virtual function void write_state(pcie_dlcmsm_state_e new_state);
+  //virtual function void write_state(pcie_dlcmsm_state_e new_state);
     // 1. Shift the history
-    prev_state = curr_state;
-    curr_state = new_state;
+    //tx_prev_state = tx_curr_state;
+    //tx_curr_state = new_state;
+    
 
     // 2. Perform state transition checks
     // TODO:  Need a way to pass pl_lnk_up and initfc flags to checks 
     
     // checks.check_init_trigger(prev_state, curr_state, pl_lnk_up);
+  //endfunction
+
+  virtual function void write_tx(pcie_dll_base_seq_item tx_item);
+    pcie_dll_dllp_seq_item tx_dllp_item;
+    pcie_dll_tlp_seq_item  tx_tlp_item;
+
+    if ($cast(tx_dllp_item, tx_item)) begin
+        `uvm_info("COMMON_CHECKS", "DLLP Item Detected - Performing Common Checks...", UVM_LOW) 
+      
+      // update cfg signals
+        checks.tx_dllp_item      = tx_dllp_item;
+
+        checks.tx_prev_state     = checks.tx_curr_state;
+        checks.tx_curr_state     = tx_dllp_item.current_state;
+        checks.tx_prev_dllp_type = checks.tx_curr_dllp_type;
+        checks.tx_curr_dllp_type = tx_dllp_item.dllp_type;
+
+  
+        // TODO: call the checks functions here for transmitted DLLP items
+        checks.tx_updated = 1'b1;
+        checks.calling_common_checks();
+
+    end
+    else if ($cast(tx_tlp_item, tx_item) && tx_tlp_item.current_state != DL_ACTIVE) begin  
+         `uvm_fatal("TRAFFIC_ISOLATION", "Violation: TLP detected while Link is NOT ACTIVE!")
+    end
+    else begin
+      `uvm_info("COMMON_CHECKS", "transmitted TLP item detected in ACTIVE state", UVM_LOW)
+      checks.tx_tlp_item   = tx_tlp_item;
+      checks.tx_prev_state = checks.tx_curr_state;
+      checks.tx_curr_state = DL_ACTIVE;
+    end
+
+    
+  endfunction
+
+
+  virtual function void write_rx(pcie_dll_base_seq_item rx_item);
+    pcie_dll_dllp_seq_item rx_dllp_item;
+    pcie_dll_tlp_seq_item  rx_tlp_item;
+
+    if ($cast(rx_dllp_item, rx_item)) begin
+        `uvm_info("COMMON_CHECKS", "DLLP Item Detected - Performing Common Checks...", UVM_LOW) 
+
+      // update cfg signals
+
+        checks.rx_dllp_item      = rx_dllp_item;
+  
+        checks.rx_prev_state     = checks.rx_curr_state;
+        checks.rx_curr_state     = rx_dllp_item.current_state;
+        checks.rx_prev_dllp_type = checks.rx_curr_dllp_type;
+        checks.rx_curr_dllp_type = rx_dllp_item.dllp_type;
+
+        checks.prev_st_count1  = checks.counter_fc1;
+        checks.prev_st_count2  = checks.counter_fc2;
+        checks.counter_fc1     = my_cfg.counter_fc1;
+        checks.counter_fc2     = my_cfg.counter_fc2;
+       
+
+        // TODO: call the checks functions here for received DLLP items
+        checks.rx_updated = 1'b1;
+        checks.calling_common_checks();
+
+    end
+    else if ($cast(rx_tlp_item, rx_item) && rx_tlp_item.current_state != DL_ACTIVE) begin  
+         `uvm_fatal("TRAFFIC_ISOLATION", "Violation: TLP detected while Link is NOT ACTIVE!")
+    end
+    else begin
+      `uvm_info("COMMON_CHECKS", "received TLP item in ACTIVE state", UVM_LOW)
+      checks.rx_tlp_item   = rx_tlp_item;
+      checks.rx_prev_state = checks.rx_curr_state;
+      checks.rx_curr_state = DL_ACTIVE;
+    end
+
+    
   endfunction
   
 endclass : pcie_dll_scoreboard
